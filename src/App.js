d51437e4a317bb19af2789c4cd4e29d5012c8222
Ahmed Bchir <ahmedbchir29@gmail.com>
2022-05-08 20:55:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { lazy, Suspense } from 'react';
+import { Route, Switch} from 'react-router-dom';
+
+import ReviewContextProvider from './context/ReviewContext';
+import TrainerContextProvider from './context/TrainerContext';
+import Lazy from './components/Lazy';
+import ErrorPage from './components/ErrorPage';
+
+
+function App() {
+  const Home = lazy(()=> import('./components/Home'));
+  const FitArmy = lazy(()=> import('./components/FitArmy'));
+  const Pricing = lazy(()=> import('./components/Pricing'));
+  const Contact = lazy(()=> import('./components/Contact'));
+  const About = lazy(()=> import('./components/About'));
+  const Produit = lazy(()=> import('./components/Produit'));
+  const IndividualExpert = lazy(()=> import('./components/IndividualExpert'));
+  const SignUp = lazy(()=> import('./components/SignUp'));
+  const ReserveExpert = lazy(()=> import('./components/ReserveExpert'));
+  return (
+          <TrainerContextProvider>
+        <ReviewContextProvider>
+          <Suspense fallback= {<Lazy/>}>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/home" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/price" component={Pricing} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/produit" component={Produit} />
+              <Route path="/register" component={SignUp} />
+              <Route path='/error' component={ErrorPage} />
+              <Route exact path="/staff/:id" component={IndividualExpert} />
+              <Route exact path="/staff/:id/reserve" component={ReserveExpert} />
+              <Route path="/fitarmy" component={FitArmy} />
+              <Route component={ErrorPage} />
+            </Switch>
+          </Suspense>
+        </ReviewContextProvider>
+      </TrainerContextProvider>
+  );
+}
+
+export default App;
